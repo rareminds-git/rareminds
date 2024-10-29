c51f618f42439374c6454209c294cf9bd9e1f017
Gaurav Girdhar <78662985+gauravgirdhar1994@users.noreply.github.com>
2024-10-29 16:34:20 +0530

    fix testimonial tabs

--- src/components/Hero/Testimonials.tsx
+++ src/components/Hero/Testimonials.tsx
@@ -1,5 +1,5 @@
+import React, { useEffect, useState } from "react";
 import { useMediaQuery } from "react-responsive";
-import TestimonialCarousel from "./TestimonalCarousel";
 import CustomTabs from "../Tabs";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
@@ -13,25 +13,32 @@ const Testimonials = ({
 }) => {
   const [tabs, setTabs] = useState();
   const isMobile = useMediaQuery({ query: `(max-width: 1200px)` });
-  console.log("categories", categories);
+
   useEffect(() => {
-    const tabsData = categories?.map((ele: any) => {
-      console.log("ele", ele);
-      return ele.Heading1;
-    });
-    console.log("Tabs data", tabsData);
-    setTabs(tabsData);
-  }, []);
+    const getData = () => {
+      console.log("in useeffect");
+      const tabsData = categories?.map((ele: any) => {
+        console.log("ele", ele);
+        return ele.Heading1;
+      });
+      console.log("Tabs data", tabsData);
+      setTabs(tabsData);
+    };
+    getData();
+  }, [categories]);
 
   console.log("tabs", tabs);
+
   return (
     <>
       <div className="xl:px-28 lg:px-20 md:px-16 px-8 xl:pb-16 lg:pb-16 md:pb-8 pb-4 xl:py-16 lg:py-20 md:py-16 py-8">
-        <CustomTabs
-          testimonials={testimonials}
-          categories={categories}
-          tabs={tabs}
-        />
+        {tabs?.length > 0 && (
+          <CustomTabs
+            testimonials={testimonials}
+            categories={categories}
+            tabs={tabs}
+          />
+        )}
       </div>
     </>
   );
--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -4,37 +4,15 @@ import TestimonialCarousel from "../Hero/TestimonalCarousel";
 
 const CustomTabs = ({ testimonials, tabs, categories }) => {
   return (
-    <Tabs className="border-0">
-      {tabs?.map((title: any) => {
-        const category = categories.filter(
-          (ele) => ele.Heading1 === title && ele
-        );
-        const testimonialData = testimonials.filter(
-          (data) => data.ContentId === category[0].ContentId && data
-        );
-        console.log("testimonials data", testimonialData);
-        return (
-          <TabPanel>
-            <h5 className="font-Syne text-center my-12 lg:mb-10 place-items-center text-[#ff2c2c] lg:text-[70px] lg:leading-[115.2px] text-[28px] leading-[30px] ">
-              Testimonials - {title}
-            </h5>
-            <TestimonialCarousel testimonials={testimonialData} />
-          </TabPanel>
-        );
-      })}
-      <TabList className="border-0 border-r-0 custom-tabs md:mx-0">
+    <Tabs preventScroll={true} className="border-0 relative pb-[50px]">
+      <TabList className="border-0 border-r-0 custom-tabs md:mx-0 absolute bottom-0">
         {tabs?.map((title: any) => {
-          const category = categories.filter(
-            (ele) => ele.Heading1 === title && ele
-          );
+          const category = categories.filter((ele) => ele.Heading1 === title);
           const testimonialData = testimonials.filter(
-            (data) => data.ContentId === category[0].ContentId && data
+            (data) => data.ContentId === category[0].ContentId
           );
           return (
-            <Tab
-              key={category[0].ContentId}
-              disabled={!testimonialData.length > 0}
-            >
+            <Tab key={title} disabled={!testimonialData.length > 0}>
               <span
                 className={
                   !testimonialData.length > 0
@@ -48,6 +26,21 @@ const CustomTabs = ({ testimonials, tabs, categories }) => {
           );
         })}
       </TabList>
+      {tabs?.map((title: any) => {
+        const category = categories.filter((ele) => ele.Heading1 === title);
+        const testimonialData = testimonials.filter(
+          (data) => data.ContentId === category[0].ContentId
+        );
+        console.log("testimonials data", testimonialData);
+        return (
+          <TabPanel key={title}>
+            <h5 className="font-Syne text-center my-12 lg:mb-10 place-items-center text-[#ff2c2c] lg:text-[70px] text-[28px] leading-[30px] ">
+              Testimonials - {title}
+            </h5>
+            <TestimonialCarousel testimonials={testimonialData} />
+          </TabPanel>
+        );
+      })}
     </Tabs>
   );
 };
