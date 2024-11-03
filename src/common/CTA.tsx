8420648b580e2bf8cf4e71818bdf8d57988c15e5
Gaurav Girdhar <78662985+gauravgirdhar1994@users.noreply.github.com>
2024-11-03 14:20:01 +0530

    fix issues for header footer ipad

--- src/common/CTA.tsx
+++ src/common/CTA.tsx
@@ -7,7 +7,7 @@ const CTA = ({ content, contactPage }) => {
   const isMobile = useMediaQuery({ query: `(max-width: 1200px)` });
   return (
     <>
-      <section className="items-center cursor-pointer mx-2 md:mx-12 lg:mx-8 xl:pb-16 lg:pb-16 md:pb-8 pb-4 xl:py-16 lg:py-20 md:py-16 py-8">
+      <section className="items-center cursor-pointer mx-2 md:mx-12 lg:mx-8 xl:mx-28 xl:pb-24 lg:pb-16 md:pb-8 pb-4 xl:py-16 lg:py-20 md:py-16 py-8">
         <div
           onClick={() => navigate("/unlock-your-potential")}
           className="border-y-2 border-[#FF2C2C] px-8 py-2 md:px-2 md:py-6 w-full sm:w-auto max-w-xs sm:max-w-none text-center sm:text-left"
